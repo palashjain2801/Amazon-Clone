1608d7ba218901d5ae8b27d3f2d8effba99d285e
palashjain2801 <palashjain2801@gmail.com>
2020-09-14 17:13:57 -0500

    Updates

---
+++ precache-manifest.9c4ed19f019ce988a7c1f959790fea3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847fd67d123b0ff343d33c1b35ffb5a8",
+    "url": "//palashjain2801.github.io/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "67463da20c9587664e0f",
+    "url": "//palashjain2801.github.io/Amazon-Clone/static/css/main.56e36172.chunk.css"
+  },
+  {
+    "revision": "03733ed2ba06f3023cfb",
+    "url": "//palashjain2801.github.io/Amazon-Clone/static/js/2.7ed2cc11.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "//palashjain2801.github.io/Amazon-Clone/static/js/2.7ed2cc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67463da20c9587664e0f",
+    "url": "//palashjain2801.github.io/Amazon-Clone/static/js/main.1692244d.chunk.js"
+  },
+  {
+    "revision": "0a09b2532fa457a8f4e6",
+    "url": "//palashjain2801.github.io/Amazon-Clone/static/js/runtime-main.f9ea996f.js"
+  }
+]);
